d0cff52787ccc5750eb04cca6b436f47101f8a1e
Zoe Laventhol <zoe.laventhol@gmail.com>
2024-01-24 13:05:31 +0100

    step #4

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+// need to import useState 
+import React, { useState } from 'react'
 import "./ImageGrid.css"
 
 // receive props in the argument of your child component function
 // you can either receive it as 'props' and then access specific props with object dot notation: 'props.images', OR you can destructure as you're accepting props: '{images}'
 export default function ImageGrid({images}) {
+  // create state "isBig"
+  const [isBig, setIsBig] = useState(false)
+
   return (
     <div className = "ImageGrid">
-      {images.map((i) => (
-          <img 
-            key={i.title}
-            src={i.url}
-            alt={i.title}
-          />
-        ))}
+      <div className = "buttons">
+        <button 
+          className={isBig ? "active" : null}
+          onClick={() => setIsBig(true)}>
+          Big
+        </button>
+        
+        <button 
+          className={!isBig ? "active" : null}
+          onClick={() => setIsBig(false)}>
+          Small
+        </button>
+      </div>
+      <div className="grid">
+        {images.map((i) => (
+            <img 
+              key={i.title}
+              src={i.url}
+              alt={i.title}
+              className={isBig ? "big" : null}
+            />
+          ))}
+        </div>
     </div>
   )
 }
