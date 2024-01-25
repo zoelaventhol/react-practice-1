2eca843bc9ecff6c8e483d2665774c670d6db61b
Zoe Laventhol <zoe.laventhol@gmail.com>
2024-01-25 23:49:50 +0100

    removed final code

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import ImageGrid from "./components/ImageGrid.jsx"
 
 function App() {
-  const emptyForm = {title: '', url: ''}
   // STATE
-  const [form, setForm] = useState(emptyForm)
-  const [images, setImages] = useState([])
-  const [showForm, setShowForm] = useState(false)
 
   // FUNCTIONS/SCRIPTS
   
-  // Save changes to input fields in "form" state
-  function handleChange(event) {
-    // capture the field name and input value where the change happened
-    let field  = event.target.name;
-    let value = event.target.value;
-
-    // update state using "setForm":
-      // use the spread operator "..." to duplicate the state that's already there
-      // use object bracket notation to update the appropriate field. 
-        // i.e. If you're typing in the "title" input field, "[field]: value" will be equivalent to "title: 'example title'"
-    setForm((state) => ({
-      ...state, 
-      [field]: value
-    }))
-  }
-
-  // Add the latest form entry to "images", then reset the form
-  function handleSubmit(event) {
-    // stops the page from refreshing and losing your data
-    event.preventDefault();
-
-    // adds your new form entry to "images" state
-    // use the spread operator "..." to duplicate the state that's already there, so you don't lose all your other images
-    setImages((state)=>([...state, form]))
-
-    // reset your form to blank
-    setForm(emptyForm)
-  }
-
   // TEMPLATE
   return (
     <div className="App">
-      <h1>Picture time! 📸</h1>
-      
-      {/* FORM */}
-      <button className = "form-btn" onClick={()=>setShowForm(!showForm)}>
-        {showForm ? "Hide form 🙈" : "Add images! 🖼️"}
-      </button>
-      
-      {showForm && 
-      <form onSubmit={handleSubmit}>
-        <div className='form-inputs'>
-          <label htmlFor='title'>Title: </label>
-          <input 
-            name='title'
-            value={form.title}
-            onChange={handleChange}
-          />
-          <br />
-          
-          <label htmlFor='url'>URL: </label>
-          <input 
-            name='url'
-            value={form.url}
-            onChange={handleChange}
-          />
-        </div>
-
-        <button>Submit</button>
-      </form>}
-      {/* END FORM */}
-      
-      {/* Pass "images" to ImageGrid as a prop */}
-      <ImageGrid images={images}/>
     </div>
   );
 }
--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,38 +1,9 @@
-// need to import useState 
-import React, { useState } from 'react'
-import "./ImageGrid.css"
+import React from 'react'
 
-// receive props in the argument of your child component function
-// you can either receive it as 'props' and then access specific props with object dot notation: 'props.images', OR you can destructure as you're accepting props: '{images}'
-export default function ImageGrid({images}) {
-  // create state "isBig"
-  const [isBig, setIsBig] = useState(false)
+export default function ImageGrid() {
 
   return (
     <div className = "ImageGrid">
-      <div className = "buttons">
-        <button 
-          className={isBig ? "active" : null}
-          onClick={() => setIsBig(true)}>
-          Big
-        </button>
-        
-        <button 
-          className={!isBig ? "active" : null}
-          onClick={() => setIsBig(false)}>
-          Small
-        </button>
-      </div>
-      <div className="grid">
-        {images.map((i) => (
-            <img 
-              key={i.title}
-              src={i.url}
-              alt={i.title}
-              className={isBig ? "big" : null}
-            />
-          ))}
-        </div>
     </div>
   )
 }
